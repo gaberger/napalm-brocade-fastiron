14b1da5e38666103f2dd1db836d3787a5af7b5df
Gary Berger <gberger@brocade.com>
2017-06-30 12:46:49 -0400

    Selective mocking tests
    Signed-off-by: Gary Berger <gberger@brocade.com>

--- test/unit/TestFastIron.py
+++ test/unit/TestFastIron.py
@@ -15,6 +15,8 @@ import textfsm
 
 from napalm_brocade_fastiron.utils.utils import send_command, read_txt_file
 
+
+
 def _send_command(command):
     
         """Wrapper for self.device.send.command().
@@ -38,15 +40,19 @@ def _send_command(command):
 
 
 class TestGetterFastIronDriver(unittest.TestCase):
+    shouldMock = True
 
     # Proxy for mocking
     def send_command(command):
         return _send_command(command)
+    
+    def skipUnlessHasattr(obj, attr):
+        if hasattr(obj, attr):
+            return lambda func: func
+        return unittest.skip("{!r} doesn't have {!r}".format(obj, attr))
 
     @classmethod
     def setUpClass(cls):
-        cls.mock = True
-
         ipaddr = "10.21.237.131"
         user = "test"
 
@@ -55,27 +61,32 @@ class TestGetterFastIronDriver(unittest.TestCase):
 
         cls.vendor = 'brocade_fastiron'
         cls.device = napalm_brocade_fastiron.fastiron.FastIronDriver(ipaddr, user, password)
-        if not cls.mock:
+        if not cls.shouldMock:
             cls.device.open()
-        
+    
+    @unittest.skipUnless(shouldMock==True, "Not Mocking")
     @mock.patch('napalm_brocade_fastiron.fastiron.FastIronDriver.send_command', side_effect=send_command)
-    def test_show_version(self, mock):
+    def test_show_version_mock(self, mock):
         result = self.device.show_version()
         self.assertEqual(result, [['08.0.40bbT211', 'ICX7450-48', 'CYX3318M0Y1', '6', '16', '35', '37']])
 
+    @unittest.skipUnless(shouldMock==True, "Not Mocking")
     @mock.patch('napalm_brocade_fastiron.fastiron.FastIronDriver.send_command', side_effect=send_command)
-    def test_show_interfaces(self, mock):
+    def test_show_interfaces_mock(self, mock):
         result = self.device.show_interfaces()
         self.assertEqual(result[0], ['GigabitEthernet1/1/1', 'disabled', 'down', '5', '17', '53', '9', '609c.9f31.b160', 'unknown', 'to196PC'])
-
-    # def test_show_version(self):
-    #     result = self.device.show_version()
-    #     # TODO Fix this because the uptime results are dynamic.
-    #     self.assertEqual(result,[[u'08.0.40bbT211', u'ICX7450-48', u'CYX3318M0Y1', u'6', u'16', u'38', u'2']])
+    
+    @unittest.skipUnless(shouldMock==False, "Mocking")
+    def test_show_version(self):
+        result = self.device.show_version()
+        # TODO Fix this because the uptime results are dynamic.
+        self.assertEqual(result,[[u'08.0.40bbT211', u'ICX7450-48', u'CYX3318M0Y1', u'6', u'16', u'38', u'2']])
     # TODO FIX THIS
-    # def test_show_interfaces(self):
-    #     result = self.device.show_interfaces()
-    #     self.assertEqual(result[0], "")
+    
+    @unittest.skipUnless(shouldMock==False, "Mocking")
+    def test_show_interfaces(self):
+        result = self.device.show_interfaces()
+        self.assertEqual(result[0], "")
 
 if __name__ == '__main__':
     unittest.main()
